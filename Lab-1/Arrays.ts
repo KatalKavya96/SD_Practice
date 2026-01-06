90976f82c7d8929ecd04e0a499bdf4fd7fa500fa
Kavya Katal <kavyakatal09@gmail.com>
2026-01-06 09:55:28 +0530

    Updating Notes

---
+++ Lab-1/Arrays.ts
@@ -0,0 +1,31 @@
+// Array of Numbers
+
+let arr : Number[] = [1,2,3,4,5];
+console.log("Array of Numbers:", arr);
+
+// Array of Strings
+
+let fruits : String[] = ["Apple", "Banana", "Orange"];
+console.log("Array of Strings:", fruits);
+
+// Mixed Array
+
+let mixedArr : (number | string | boolean)[] = [1, "Two", true, 4, "Five", false];
+console.log("Mixed Array:", mixedArr);
+
+// Array of Objects
+
+let users : { id: number; name: string }[] = [
+  { id: 1, name: "Kavya" },
+  { id: 2, name: "Samarya" },
+];
+console.log("Array of Objects:", users);
+
+// Multi-dimensional Array
+
+let matrix : number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log("Multi-dimensional Array:", matrix);
---
+++ Lab-1/OptionalParameter.ts
@@ -0,0 +1,6 @@
+function greet(name?: string): string {
+  return `Hello, ${name}`;
+}
+console.log("Function greet():", greet("Kavya"));
+
+// ? = If age then
