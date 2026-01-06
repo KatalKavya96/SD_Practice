5452d975a75022db0f42d59d8eab04d9fc11181b
Kavya Katal <kavyakatal09@gmail.com>
2026-01-06 09:39:59 +0530

    Updating Notes

---
+++ Lab-1/hello_1.ts
@@ -0,0 +1,81 @@
+let message = "Hello Typescript"
+console.log(message)
+
+
+// Datatypes
+
+// Number
+// Boolean
+// String
+// Object
+// null
+// undefined
+// Interface
+
+// variable name : type = value
+
+let text : String = "TSx";
+console.log(text)
+
+// =======================
+// Number
+// =======================
+let age: number = 21;
+console.log("Number:", age);
+
+// =======================
+// Boolean
+// =======================
+let isLoggedIn: boolean = true;
+console.log("Boolean:", isLoggedIn);
+
+// =======================
+// String
+// =======================
+let username: string = "Kavya";
+console.log("String:", username);
+
+// =======================
+// Object
+// =======================
+let user: {
+  id: number;
+  name: string;
+  isActive: boolean;
+} = {
+  id: 1,
+  name: "Kavya",
+  isActive: true,
+};
+console.log("Object:", user);
+
+// =======================
+// null
+// =======================
+let data: null = null;
+console.log("null:", data);
+
+// =======================
+// undefined
+// =======================
+let value: undefined = undefined;
+console.log("undefined:", value);
+
+// =======================
+// Interface
+// =======================
+interface Student {
+  rollNo: number;
+  name: string;
+  course: string;
+  isEnrolled: boolean;
+}
+
+let student: Student = {
+  rollNo: 101,
+  name: "Samarya",
+  course: "Computer Science",
+  isEnrolled: true,
+};
+
+console.log("Interface Object:", student);
