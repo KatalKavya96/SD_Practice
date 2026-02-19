b5ac13fd4f8101632612a7b86716e49136e8cf22
Kavya Katal <kavyakatal09@gmail.com>
2026-02-19 10:10:51 +0530

    Vehicle Factory Question Completion

--- Question(Singleton+Factory)/Question2.ts
+++ Question(Singleton+Factory)/Question2.ts
@@ -4,20 +4,52 @@ interface Vehicle {
 
 class Car implements Vehicle {
 //todo
+
+    // private client : Vehicle;
+    // constructor(client:Vehicle){
+
+    //     this.client=client
+    // }
+    start():string{
+
+        return "Car has started"
+    }
  }
 
 class Bike implements Vehicle {
   //todo
+
+  start(): string {
+      return "Bike has started"
+  }
 }
 
 class Truck implements Vehicle {
   //todo
+
+  start():string{
+
+    return "Truck has started"
+  }
 }
 
 type VehicleType = "car" | "bike" | "truck";
 
 class VehicleFactory {
  //todo
+
+ static createVehicle(type: VehicleType): Vehicle{
+    switch(type){
+        case "car":
+            return new Car()
+        case "bike":
+            return new Bike()
+        case "truck":
+            return new Truck()
+        default:
+            throw new Error("Invalid Vehicle Type")
+    }
+ }
 }
 
 const car = VehicleFactory.createVehicle("car");
@@ -29,5 +61,6 @@ console.log(bike.start());
 const truck = VehicleFactory.createVehicle("truck");
 console.log(truck.start());
 
+// This question has been successfully completed.
 
 
